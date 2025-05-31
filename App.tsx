74896da1b921fce889536360b1545bcbceec9aa3
alieron <sebastien.leib1@gmail.com>
2025-06-01 03:46:27 +0800

    Add screens and functionality for navbar buttons to navigate to the screens

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import './index.css';
-import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './screens/Home';
+import FloodMap from './screens/FloodMap';
+import FloodBot from './screens/FloodBot';
+import SubmitReport from './screens/SubmitReport';
+import Settings from './screens/Settings';
 import BottomNavBar from './components/BottomNavBar';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-center text-red-500 text-lg">
-        Open up App.tsx to start working on your app!
-      </Text>
-      <BottomNavBar />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ headerShown: false }}
+        tabBar={() => <BottomNavBar />} // remove and add BottomNavBar to select screens if there are other screens u dont want to have navbar on
+      >
+        {/* Default screen */}
+        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="FloodMap" component={FloodMap}/>
+        <Tab.Screen name="SubmitReport" component={SubmitReport}/>
+        <Tab.Screen name="FloodBot" component={FloodBot}/>
+        <Tab.Screen name="Settings" component={Settings}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
