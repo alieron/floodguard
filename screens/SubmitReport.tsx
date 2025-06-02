59a63ec6fe97353ea2d3b3211d63cd4e8e6b4abc
Tenia <toktenia@gmail.com>
2025-06-02 20:59:47 +0800

    Add text details to first box

--- screens/SubmitReport.tsx
+++ screens/SubmitReport.tsx
@@ -3,11 +3,17 @@ import { Text, View } from 'react-native';
 export default function SubmitReport() {
   return (
     <View className="flex-1 justify-center items-center bg-blue-300">
-      <Text className="text-center text-black text-lg">
+      <Text className="text-center text-black text-lg text-3xl">
         SUBMIT A REPORT
       </Text>
       <View className="bg-white w-10/12 h-1/5 rounded-2xl">
         <Text className="text-3xl bg-blue-600 rounded-t-2xl p-2 text-center text-white">Step 1: Upload image</Text>
+        <View className="items-center h-full w-full">
+          <Text className="p-2">Take a picture or choose from your library.</Text>
+          <View className="rounded-2xl ring-black ring-2 w-11/12 h-1/2">
+
+          </View>
+        </View>
       </View>
     </View>
   );
