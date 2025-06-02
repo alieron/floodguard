5c75480520990caab3914ffcb28b9294b1453674
faithyap <e1354368@u.nus.edu>
2025-06-02 22:53:35 +0800

    changed the colour of the settings screen to blue

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -2,9 +2,9 @@ import { Text, View } from 'react-native';
 
 export default function Settings() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-center text-red-500 text-lg">
-        Settings
+    <View className="flex-1 justify-center items-center bg-blue-300">
+      <Text className="text-center text-black text-2xl">
+        SETTINGS
       </Text>
     </View>
   );
