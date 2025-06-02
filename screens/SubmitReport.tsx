99f19b13fe9be6026a6300b050a8ed9b95debeed
Tenia <toktenia@gmail.com>
2025-06-02 20:44:49 +0800

    Create first basic box component in submit report

--- screens/SubmitReport.tsx
+++ screens/SubmitReport.tsx
@@ -2,10 +2,13 @@ import { Text, View } from 'react-native';
 
 export default function SubmitReport() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-center text-red-500 text-lg">
-        SubmitReport
+    <View className="flex-1 justify-center items-center bg-blue-300">
+      <Text className="text-center text-black text-lg">
+        SUBMIT A REPORT
       </Text>
+      <View className="bg-white w-10/12 h-1/5 rounded-2xl">
+        <Text className="text-3xl bg-blue-600 rounded-t-2xl p-2 text-center text-white">Step 1: Upload image</Text>
+      </View>
     </View>
   );
 }
