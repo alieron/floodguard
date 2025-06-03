7e95ee1010c06c8c6bd2985bb94bbf0de620c940
Tenia <toktenia@gmail.com>
2025-06-03 16:21:43 +0800

    Create location box and submit button

--- components/ReportFormBox.tsx
+++ components/ReportFormBox.tsx
@@ -13,7 +13,7 @@ export default function ReportFormBox({
   children,
 }: ReportFormBoxProps) {
   return (
-      <View className="bg-white w-10/12 h-1/5 rounded-2xl m-5">
+      <View className="bg-white w-10/12 h-1/4 rounded-2xl m-5">
         <Text className="text-3xl bg-blue-600 rounded-t-2xl p-2 text-center text-white">
           {headerText}
         </Text>
@@ -21,7 +21,7 @@ export default function ReportFormBox({
           <Text className="p-2">
             {subtitle}
           </Text>
-          <View className="rounded-2xl ring-black ring-2 w-11/12 h-1/2 justify-center items-center">
+          <View className="rounded-2xl ring-black ring-2 w-11/12 h-3/5 justify-center items-center">
             {children}
           </View>
         </View>
--- screens/SubmitReport.tsx
+++ screens/SubmitReport.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TouchableOpacity, Alert, Button } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import * as Location from "expo-location";
 import React, { useState } from "react";
@@ -6,53 +6,42 @@ import ReportFormBox from "../components/ReportFormBox";
 
 export default function SubmitReport() {
   return (
-    <View className="flex-1 justify-center items-center bg-blue-300">
-      <Text className="text-center text-black text-lg text-3xl">
-        SUBMIT A REPORT
-      </Text>
-      
+    <View className="flex-1 items-center bg-blue-300">
+      <View className="w-full pt-20 items-center justify-center border-gray-900">
+        <Text className="text-center text-black text-3xl">SUBMIT A REPORT</Text>
+      </View>
+
       <ReportFormBox
         headerText="Step 1: Upload Image"
         subtitle="Take a picture or choose from your library."
       >
         <View className="w-full h-full flex-row justify-center items-center">
-          <TouchableOpacity>
-            <Ionicons name="camera-outline" size={50} margin={20} />
+          <TouchableOpacity className="justify-center items-center mr-10">
+            <Ionicons name="camera-outline" size={50} />
+            <Text>Camera</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Ionicons name="image-outline" size={50} margin={20} />
+          <TouchableOpacity className="justify-center items-center ml-10">
+            <Ionicons name="image-outline" size={50} />
+            <Text>Gallery</Text>
           </TouchableOpacity>
         </View>
       </ReportFormBox>
 
-      <View className="bg-white w-10/12 h-1/5 rounded-2xl m-5">
-        <Text className="text-3xl bg-blue-600 rounded-t-2xl p-2 text-center text-white">
-          Step 2: Select Location
-        </Text>
-        <View className="items-center h-full w-full">
-          <Text className="p-2">Select location or use current location.</Text>
-          <View className="rounded-2xl ring-black ring-2 w-11/12 h-1/2 justify-center items-center flex-row"></View>
+      <ReportFormBox
+        headerText="Step 2: Select Location"
+        subtitle="Use current location or select location."
+      >
+        <View className="w-full h-full justify-center items-center">
+          <TouchableOpacity className="bg-blue-600 rounded p-2">
+            <Text className="text-white">Detect location</Text> {/* how... */}
+          </TouchableOpacity>
+          {/* another option of manually searching your location? */}
         </View>
-      </View>
+      </ReportFormBox>
 
-      <View className="bg-white w-10/12 h-1/5 rounded-2xl m-5">
-        <Text className="text-3xl bg-blue-600 rounded-t-2xl p-2 text-center text-white">
-          Step 2: Select Location
-        </Text>
-        <View className="items-center h-full w-full">
-          <Text className="p-2">
-            Take a picture or choose from your library.
-          </Text>
-          <View className="rounded-2xl ring-black ring-2 w-11/12 h-1/2 justify-center items-center flex-row">
-            <TouchableOpacity>
-              <Ionicons name="camera-outline" size={50} margin={20} />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Ionicons name="image-outline" size={50} margin={20} />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+      <TouchableOpacity className="bg-blue-600 rounded-2xl p-2 m-5 w-10/12 h-15">
+        <Text className="text-white text-center text-3xl">SUBMIT</Text>
+      </TouchableOpacity>
     </View>
   );
 }
