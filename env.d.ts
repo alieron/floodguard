11fc81f03fa6ec526da81569af14157fd932eef9
JunhaoW3 <wupillow@gmail.com>
2025-06-08 02:56:10 +0800

    Implemented basic chatbot system without system prompt to guide the conversation

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,8 @@
 declare module '@env' {
   export const APPWRITE_PROJECT_ID: string;
-	export const APPWRITE_DATABASE_ID: string;
+  export const APPWRITE_DATABASE_ID: string;
   export const APPWRITE_IMAGE_BUCKET_ID: string;
+  export const DIALOGFLOW_SERVICE_ACC: string;
+  export const DIALOGFLOW_PRIVATE_KEY: string;
+  export const DIALOGFLOW_PROJECT_ID: string;
 }
--- screens/FloodBot.tsx
+++ screens/FloodBot.tsx
@@ -1,11 +1,66 @@
 import { Text, View } from 'react-native';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+import React, { useCallback, useState } from 'react';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 
-export default function FloodBot() {
+import {
+  DIALOGFLOW_SERVICE_ACC,
+  DIALOGFLOW_PRIVATE_KEY,
+  DIALOGFLOW_PROJECT_ID
+} from '@env';
+
+Dialogflow_V2.setConfiguration(
+  DIALOGFLOW_SERVICE_ACC,
+  DIALOGFLOW_PRIVATE_KEY,
+  'en',
+  DIALOGFLOW_PROJECT_ID,
+);
+
+interface DialogflowResult {
+  queryResult: {
+    fulfillmentText: string;
+  };
+}
+
+function FloodBot() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const onSend = (newMessages: IMessage[] = []) => {
+    setMessages(prev => GiftedChat.append(prev, newMessages));
+    const userMessage = newMessages[0].text;
+    Dialogflow_V2.requestQuery(
+      userMessage,
+      (result: object) => {
+        // Handle chatbot response
+        const res = result as DialogflowResult;
+        const chatbotResponse = res.queryResult.fulfillmentText;
+        // Add the chatbot's response to the chat interface
+        const botMessage = {
+          _id: Math.random().toString(36).substring(7),
+          text: chatbotResponse,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Floodbot',
+            avatar: 'https://imgur.com/7k12EPD' //Can replace this with FloodGuard image
+          },
+        };
+        setMessages(previousMessages => GiftedChat.append(previousMessages, [botMessage]));
+      },
+      error => {
+        // Handle errors
+        console.error('DialogFlow error:', error);
+      }
+    );
+  };
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-center text-red-500 text-lg">
-        FloodBot
-      </Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={newMessages => onSend(newMessages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 }
+
+export default FloodBot();
