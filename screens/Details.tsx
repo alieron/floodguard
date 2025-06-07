398511b70ed7fda9328b1f67d1b5b66aa589cbd6
alieron <sebastien.leib1@gmail.com>
2025-06-07 16:08:59 +0800

    Add dynamic title to detail allowing it to be both Warning and Report

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 
 export default function ReportScreen({ navigation, route }: any) {
   const [report, setReport] = useState();
-  const id = route.params.id;
+  const { id, type }: { id: string, type: string } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: type.charAt(0).toUpperCase() + type.slice(1)
+    })
+  }, [navigation, type])
 
   return (
     <View className="flex-1 bg-white mb-20">
