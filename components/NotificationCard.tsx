da851ea0003611df74da60245dabe9b3a42abb2b
alieron <sebastien.leib1@gmail.com>
2025-06-07 16:09:45 +0800

    Add navigation to details page through notification cards

--- components/NotificationCard.tsx
+++ components/NotificationCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { View, Text, ScrollView, Image, NativeSyntheticEvent, NativeScrollEvent, Dimensions } from 'react-native';
+import { View, Text, ScrollView, Pressable, Image, NativeSyntheticEvent, NativeScrollEvent, Dimensions } from 'react-native';
+import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 
 const screenWidth = Dimensions.get("window").width;
@@ -67,12 +68,17 @@ const ImageSlider = ({ images }: { images: string[] }) => {
 };
 
 const NotificationCard = ({ item }: { item: NotificationCardProps }) => {
+	const navigation = useNavigation<NavigationProp<ParamListBase>>();
+
 	const typeLabel = item.type === "warning"
 		? (<><Ionicons name="warning" size={20} color="red" /><Text>Warning</Text></>)
 		: (<><Ionicons name="pin" size={20} color="blue" /><Text>Report</Text></>)
 
 	return (
-		<View className="bg-white rounded-2xl p-4 mb-4 shadow-sm">
+		<Pressable
+			className="bg-white rounded-2xl p-4 mb-4 shadow-sm"
+			onPress={() => navigation.navigate("Details", { id: item.id, type: item.type })}
+		>
 			<View className="flex flex-row items-center text-lg font-bold mb-2">{typeLabel}</View>
 
 			<ImageSlider images={item.imageUrls} />
@@ -81,7 +87,7 @@ const NotificationCard = ({ item }: { item: NotificationCardProps }) => {
 			<Text className="mt-1 text-xs text-gray-500 text-right">
 				{formatDate(item.reportedAt)}
 			</Text>
-		</View>
+		</Pressable>
 	);
 };
 
